1bc8ba01a29018fe5d00802a304c013a82b81824
keerthukaviya29 <146410018+keerthukaviya29@users.noreply.github.com>
2025-09-30 23:56:41 +0530

    output.py

---
+++ output.py
@@ -0,0 +1,33 @@
+--- Flight Reservation System ---
+1. Passenger
+2. Cashier
+3. Exit
+Select user type: 1
+
+Passenger Options:
+1. Sign Up
+2. Log In
+3. Exit to Main Menu
+Choose an option: 1
+Enter new username: john
+Enter password: ****
+Account created successfully!
+
+Passenger Options:
+1. Sign Up
+2. Log In
+3. Exit to Main Menu
+Choose an option: 2
+Enter username: john
+Enter password: ****
+Welcome back, john!
+
+Passenger Menu (john):
+1. Check Availability & Fare
+2. Book Ticket
+3. Log Out
+Choose an option: 1
+Available Flights:
+AI101: Delhi - Seats: 5 - Fare: 5000
+AI102: Mumbai - Seats: 3 - Fare: 4500
+AI103: Chennai - Seats: 4 - Fare: 4000
